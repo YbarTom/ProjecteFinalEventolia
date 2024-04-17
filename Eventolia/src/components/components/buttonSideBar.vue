386c8fdcae50a8c87fba2ddf52fdb5040ed641db
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-17 12:42:54 +0200

    <

---
+++ Eventolia/src/components/components/buttonSideBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="button bg-warning">
+  </div>
+</template>
+
+<script setup>
+  // Aquí puedes añadir tu lógica JavaScript si es necesario
+</script>
+
+<style scoped>
+.button {
+  height: 55px;
+  width: 100%;
+}
+
+</style>
