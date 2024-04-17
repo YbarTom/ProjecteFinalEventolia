83e66453bde86e49123f575a67502cadf55549d4
Andrés <a22miqdervil@inspedralbes.cat>
2024-04-17 12:44:25 +0200

    yipiyou

---
+++ server/events.js
@@ -0,0 +1,19 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const DBurl = "mongodb://a22miqdervil:Pedralbes23@ac-nzxncrt-shard-00-00.yfxmjwr.mongodb.net:27017,ac-nzxncrt-shard-00-01.yfxmjwr.mongodb.net:27017,ac-nzxncrt-shard-00-02.yfxmjwr.mongodb.net:27017/?replicaSet=atlas-g6ezpq-shard-0&ssl=true&authSource=admin";
+const DBname = "EventoliaFinal";
+
+const client = new MongoClient(DBurl, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+        poolSize: 15
+    }
+});
+
+const database = client.db(DBname);
+usersCollection = database.collection("Events")
+
+module.exports = {
+}
