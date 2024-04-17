5668537ae9bfc8bde37a568318d19984d14fd666
Andrés <a22miqdervil@inspedralbes.cat>
2024-04-17 11:44:16 +0200

    server

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+
+const usersDB = require("./users.js")
+
+app.use(bodyParser.json());
+app.use(
+    cors({
+        origin: "*",
+        credentials: true
+    })
+);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log("Server running on port: ", PORT);
+});
+
+const DBurl = "mongodb://a22miqdervil:Pedralbes23@ac-nzxncrt-shard-00-00.yfxmjwr.mongodb.net:27017,ac-nzxncrt-shard-00-01.yfxmjwr.mongodb.net:27017,ac-nzxncrt-shard-00-02.yfxmjwr.mongodb.net:27017/?replicaSet=atlas-g6ezpq-shard-0&ssl=true&authSource=admin";
+const DBname = "EventoliaFinal";
+
+const client = new MongoClient(DBurl, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+        poolSize: 15
+    }
+});
+
+module.exports = { DBurl, DBname, client };
+
+app.get("/getUsers", async (req, res) => {
+    try {
+        const users = await usersDB.getUsers()
+        res.send(users)
+    } catch (error) {
+        console.error("Error getting users", error)
+    }
+})
+
+
+
+
+
