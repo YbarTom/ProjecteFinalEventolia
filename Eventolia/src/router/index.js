c3bcebe1271e48233e567e966a3416c0a37f63ee
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-17 09:31:49 +0200

    ruta

--- Eventolia/src/router/index.js
+++ Eventolia/src/router/index.js
@@ -1,16 +1,23 @@
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
-
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/pages/index.vue'),
+  },
+  {
+    path: '/MainPage',
+    name: 'MainPage',
+    component: () => import('@/pages/MainPage.vue'),
+  },
+
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  extendRoutes: setupLayouts,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 })
 
 export default router
