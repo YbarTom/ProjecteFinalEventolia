d4cea329e8d26219e05a206db920a10f2bf70933
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-17 11:52:57 +0200

    a

--- Eventolia/src/components/components/LeftSideMainPage.vue
+++ Eventolia/src/components/components/LeftSideMainPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container bg-text">
-
+    <div class="leftBar"></div>
   </div>
 </template>
 
@@ -13,6 +13,12 @@
     height: 100vh;
   }
 
+  .leftBar {
+    width: 54%;
+    height: 100vh;
+    background-color: #f8f9fa;
+    border-right: 1px solid #e9ecef;
+  }
 
 
 
