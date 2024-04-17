536b9e8544ad66e36f0623f4029c0f526e4b2dfe
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-17 12:01:53 +0200

    right bar inside left bar

--- Eventolia/src/components/components/LeftSideMainPage.vue
+++ Eventolia/src/components/components/LeftSideMainPage.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="container ">
-    <div class="leftBar"></div>
+  <div class="container">
+    <div class="leftBar">
+      <div class="bar bg-text"></div>
+    </div>
   </div>
 </template>
 
 <script setup>
-  //
+  // Aquí puedes añadir tu lógica JavaScript si es necesario
 </script>
 
 <style scoped>
   .container {
     height: 100vh;
-
   }
 
   .leftBar {
     width: 54%;
-    height: 100vh;
-    border-right: 1px solid #3F0D44;
+    height: 100%;
+    position: relative; /* Añadimos position: relative para posicionar la barra absolutamente dentro de este div */
   }
 
-
-
+  .bar {
+    width: 1px; /* Cambiamos el width para que sea una barra fina */
+    height: 100%;
+    position: absolute; /* La posicionamos absolutamente dentro de leftBar */
+    right: 0; /* La movemos hacia la derecha del contenedor */
+    top: 0; /* Alineamos la barra vertical arriba del contenedor */
+  }
 </style>
