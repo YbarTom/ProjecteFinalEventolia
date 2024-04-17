934d3018eb7414ebf7eac5b1ef53f374084cb1f4
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-17 09:46:41 +0200

    dividido entre tres

--- Eventolia/src/components/ViewComponents/Desktop/DesktopMainPage.vue
+++ Eventolia/src/components/ViewComponents/Desktop/DesktopMainPage.vue
@@ -1,7 +1,34 @@
 <template>
-  <h1>hola</h1>
+  <div class="container">
+    <div class="left-panel"></div>
+    <div class="middle-panel"></div>
+    <div class="right-panel"></div>
+  </div>
 </template>
 
 <script setup>
   //
 </script>
+
+<style scoped>
+  .container {
+    display: flex;
+    height: 100vh;
+  }
+
+  .left-panel, .middle-panel, .right-panel {
+    flex: 1;
+  }
+
+  .left-panel {
+    background-color: blue;
+  }
+
+  .middle-panel {
+    background-color: green;
+  }
+
+  .right-panel {
+    background-color: red;
+  }
+</style>
