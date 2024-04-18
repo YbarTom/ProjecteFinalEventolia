354942209542c054e5edda752697d58b8d22cb15
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-18 12:16:45 +0200

    soy el puto amo

--- Eventolia/src/components/components/buttonSideBar.vue
+++ Eventolia/src/components/components/buttonSideBar.vue
@@ -18,20 +18,38 @@ import home from '../../assets/iconsImages/home.svg';
 import user from '../../assets/iconsImages/user.svg';
 import add from '../../assets/iconsImages/add.svg';
 import message from '../../assets/iconsImages/message.svg';
+import lupaWhite from '../../assets/iconsImages/lupaWhite.svg';
+import bellWhite from '../../assets/iconsImages/bellWhite.svg';
+import homeWhite from '../../assets/iconsImages/homeWhite.svg';
+import userWhite from '../../assets/iconsImages/userWhite.svg';
+import addWhite from '../../assets/iconsImages/addWhite.svg';
+import messageWhite from '../../assets/iconsImages/messageWhite.svg';
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
 
 const props = defineProps({
   type: String,
   buttonText: String
 });
 
-// Definimos la ruta del SVG basada en el tipo proporcionado
+// Definimos la ruta del SVG basada en el tipo proporcionado y el tema actual
 let svgPath;
-if (props.type === 'lupa') svgPath = lupa;
-else if (props.type === 'bell') svgPath = bell;
-else if (props.type === 'home') svgPath = home;
-else if (props.type === 'user') svgPath = user;
-else if (props.type === 'add') svgPath = add;
-else if (props.type === 'message') svgPath = message;
+if (theme.global.current._value.dark) {
+  if (props.type === 'lupa') svgPath = lupaWhite;
+  else if (props.type === 'bell') svgPath = bellWhite;
+  else if (props.type === 'home') svgPath = homeWhite;
+  else if (props.type === 'user') svgPath = userWhite;
+  else if (props.type === 'add') svgPath = addWhite;
+  else if (props.type === 'message') svgPath = messageWhite;
+} else {
+  if (props.type === 'lupa') svgPath = lupa;
+  else if (props.type === 'bell') svgPath = bell;
+  else if (props.type === 'home') svgPath = home;
+  else if (props.type === 'user') svgPath = user;
+  else if (props.type === 'add') svgPath = add;
+  else if (props.type === 'message') svgPath = message;
+}
 </script>
 
 <style scoped>
@@ -51,8 +69,8 @@ else if (props.type === 'message') svgPath = message;
   /* Ajusta el espacio entre el icono y el texto */
 }
 
-h1 {
+h3 {
   margin: 0;
-  /* Elimina el margen predeterminado del h1 */
+  /* Elimina el margen predeterminado del h3 */
 }
 </style>
