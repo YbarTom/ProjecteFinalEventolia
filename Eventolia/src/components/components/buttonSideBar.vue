f822f78e0085d4e948fc8922cff47d9c95ad1371
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-18 11:47:49 +0200

    left bar

--- Eventolia/src/components/components/buttonSideBar.vue
+++ Eventolia/src/components/components/buttonSideBar.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="button ">
+  <div class="button">
     <div class="svg-container">
-      <img src="../../assets/iconsImages/lupa.svg" alt="SVG Icon" />
-      <h1>BUSCAR</h1>
+      <!-- Utilizamos la imagen de la lupa directamente -->
+      <img :src="svgPath" alt="SVG Icon" />
     </div>
+    <h3 class="text-text">{{ buttonText }}</h3>
   </div>
 </template>
 
 <script setup>
-  // Aquí puedes añadir tu lógica JavaScript si es necesario
+import { defineProps } from 'vue';
+
+// Importamos las imágenes
+import lupa from '../../assets/iconsImages/lupa.svg';
+import bell from '../../assets/iconsImages/bell.svg';
+import home from '../../assets/iconsImages/home.svg';
+import user from '../../assets/iconsImages/user.svg';
+import add from '../../assets/iconsImages/add.svg';
+import message from '../../assets/iconsImages/message.svg';
+
+const props = defineProps({
+  type: String,
+  buttonText: String
+});
+
+// Definimos la ruta del SVG basada en el tipo proporcionado
+let svgPath;
+if (props.type === 'lupa') svgPath = lupa;
+else if (props.type === 'bell') svgPath = bell;
+else if (props.type === 'home') svgPath = home;
+else if (props.type === 'user') svgPath = user;
+else if (props.type === 'add') svgPath = add;
+else if (props.type === 'message') svgPath = message;
 </script>
 
 <style scoped>
@@ -22,6 +45,14 @@
 .svg-container {
   height: 55px;
   width: 55px;
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+  /* Ajusta el espacio entre el icono y el texto */
 }
 
+h1 {
+  margin: 0;
+  /* Elimina el margen predeterminado del h1 */
+}
 </style>
--- Eventolia/src/components/components/sideBar.vue
+++ Eventolia/src/components/components/sideBar.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="container">
-    <buttonSideBar/>
+  <div class="button-side-bar">
+    <!-- Usamos el componente Button con las rutas y textos pasados como atributos -->
+    <ButtonSideBar type="home" buttonText="Home" />
+    <ButtonSideBar type="lupa" buttonText="Search" />
+    <ButtonSideBar type="add" buttonText="Create" />
+    <ButtonSideBar type="message" buttonText="Message" />
+    <ButtonSideBar type="bell" buttonText="Notification" />
+    <ButtonSideBar type="user" buttonText="Profile" />
+
+
   </div>
 </template>
 
 <script setup>
-  // Aquí puedes añadir tu lógica JavaScript si es necesario
-  import buttonSideBar from './buttonSideBar.vue';
+import ButtonSideBar from './buttonSideBar.vue';
+
+// Aquí puedes añadir tu lógica JavaScript si es necesario
 </script>
 
 <style scoped>
-.container{
-  height:100vh;
+.button-side-bar {
   margin-left: 20px;
-  width:calc(100% - 20px);
 }
 </style>
