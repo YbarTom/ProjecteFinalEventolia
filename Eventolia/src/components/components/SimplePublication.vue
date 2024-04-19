226fce59a78e92d1536eafef4e8edc3a0d8c0d52
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-19 12:06:42 +0200

    images ok

--- Eventolia/src/components/components/SimplePublication.vue
+++ Eventolia/src/components/components/SimplePublication.vue
@@ -1,36 +1,57 @@
 <template>
-  <div class="div-exterior bg-principal">
+  <div ref="divExterior" class="div-exterior bg-principal">
     <div class="profile-div bg-warning"></div>
-    <img src="../../assets/images/rascacielos.jpeg" alt="Imagen publicacion" class="profile-img">
+    <img ref="image" class="profile-img" src="../../assets/images/rascacielos.jpeg" alt="Imagen de perfil" @load="adjustHeight" />
+    <div ref="commentsDiv" class="comments-div bg-warning"></div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const divExterior = ref(null);
+const image = ref(null);
+const commentsDiv = ref(null);
+
+const adjustHeight = () => {
+  if (image.value && commentsDiv.value && divExterior.value) {
+    const totalHeight = image.value.offsetHeight + commentsDiv.value.offsetHeight;
+    const clampedHeight = Math.min(Math.max(totalHeight, 400), 750); // Limitar la altura entre 400 y 750 píxeles
+    divExterior.value.style.height = `${clampedHeight}px`;
+  }
+}
 </script>
 
 <style scoped>
 .div-exterior {
   margin-top: 10px;
   border-radius: 8px;
-  height: 600px;
   width: 468px;
-  position: relative; /* Añadimos posición relativa para que la imagen se posicione correctamente */
-  overflow: hidden; /* Ocultamos cualquier parte de la imagen que se extienda más allá del contenedor */
+  position: relative;
+  overflow: hidden;
 }
 
 .profile-div {
   width: 100%;
   height: 65px;
-  border-radius: 8px 8px 0px 0px;
+  border-radius: 8px 8px 0 0;
+}
+
+.comments-div {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+  border-radius: 0 0 8px 8px;
 }
 
 .profile-img {
-  position: absolute; /* Posicionamos la imagen absolutamente dentro del contenedor */
-  top: 65px; /* Colocamos la imagen debajo del div .profile-div */
+  position: absolute;
+  top: 65px;
   left: 0;
-  width: 100%; /* Ajustamos el ancho de la imagen al 100% del contenedor */
-  height: auto; /* Permitimos que la altura de la imagen se ajuste automáticamente */
-  max-height: calc(100% - 65px); /* Establecemos la altura máxima de la imagen al espacio restante */
-  max-width: 100%; /* Establecemos el ancho máximo de la imagen al 100% del contenedor */
+  width: 100%;
+  height: auto;
+  max-height: calc(100% - 65px);
+  max-width: 100%;
 }
 </style>
