bc31ddae5cf0d7c98b005e1881a981d9a7e1bca5
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-22 10:32:51 +0200

    icons mdi

--- Eventolia/src/components/components/buttonPublication.vue
+++ Eventolia/src/components/components/buttonPublication.vue
@@ -1,40 +1,27 @@
 <template>
   <div class="svg-container">
-    <v-icon icon="mdi-heart" class="text-text" />
-
+    <v-icon :icon="icono" class="text-text" />
   </div>
 </template>
 
 <script setup>
-import heartWhite from '../../assets/iconsImages/heartWhite.svg';
-import saveWhite from '../../assets/iconsImages/saveWhite.svg';
-import chatWhite from '../../assets/iconsImages/chatWhite.svg';
-import sendWhite from '../../assets/iconsImages/sendWhite.svg';
-import heart from '../../assets/iconsImages/heart.svg';
-import save from '../../assets/iconsImages/save.svg';
-import chat from '../../assets/iconsImages/chat.svg';
-import send from '../../assets/iconsImages/send.svg';
-import { useTheme } from "vuetify";
-import { defineProps } from 'vue';
-
-const theme = useTheme();
+import { defineProps, ref } from 'vue';
 
 const props = defineProps({
   type: String,
   buttonText: String
 });
 
-let svgPath;
-if (theme.global.current._value.dark) {
-  if (props.type === 'heart') svgPath = heartWhite;
-  else if (props.type === 'save') svgPath = saveWhite;
-  else if (props.type === 'chat') svgPath = chatWhite;
-  else if (props.type === 'send') svgPath = sendWhite;
-} else {
-  if (props.type === 'heart') svgPath = heart;
-  else if (props.type === 'save') svgPath = save;
-  else if (props.type === 'chat') svgPath = chat;
-  else if (props.type === 'send') svgPath = send;
+const icono = ref('');
+
+if (props.type === 'heart') {
+  icono.value = "mdi-heart-outline";
+}else if (props.type === 'chat') {
+  icono.value = "mdi-chat-outline";
+}else if (props.type === 'send') {
+  icono.value = "mdi-send";
+}else if (props.type === 'save') {
+  icono.value = "mdi-bookmark-outline";
 }
 </script>
 
@@ -42,7 +29,6 @@ if (theme.global.current._value.dark) {
 .svg-container {
   display: flex;
   align-items: center;
-  margin-right: 10px;
-  /* Ajusta el espacio entre el icono y el texto */
+  margin-right: 10px; /* Ajusta el espacio entre el icono y el texto */
 }
 </style>
