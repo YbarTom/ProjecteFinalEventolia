fd10ec984b0ef89e14b1ffc523bed97d745a85e5
Andrés <a22miqdervil@inspedralbes.cat>
2024-04-23 10:16:12 +0200

    a

--- Eventolia/src/components/ViewComponents/Desktop/DesktopUserProfilePage.vue
+++ Eventolia/src/components/ViewComponents/Desktop/DesktopUserProfilePage.vue
@@ -7,19 +7,31 @@
   
   <script setup>
   import LeftSideMainPage from "@/components/components/LeftSideMainPage.vue";
-  import UserSideUserProfilePage from '@/components/components/'
+  import UserSideUserProfilePage from '@/components/components/UserProfile/UserSideUserProfilePage.vue'
   </script>
   
-  <style scoped>
-    .container {
-      display: flex;
-      height: 100vh;
-    }
-  
-    .left-panel, .middle-panel, .right-panel {
-      flex: 1;
-    }
-  
-  
-  </style>
+<style scoped>
+  .container {
+    display: flex;
+    height: 100vh;
+  }
+
+  .left-panel {
+    flex: 1;
+    overflow: auto; /* para el manejo de desbordamientos de contenido */
+    min-width: 635px; /* No tocar es para que tenga el mismo tamaño que en la barra de la pagina Main*/
+  }
+
+  .resizer {
+    cursor: ew-resize;
+    background-color: #ccc;
+    width: 5px;
+  }
+
+  .user-side {
+    flex: 3;
+    overflow: auto;
+  }
+</style>
+
   
