ffc864c7473c5545fff07bdcf5ad1e71c5c25169
Andrés <a22miqdervil@inspedralbes.cat>
2024-04-23 10:23:48 +0200

    A

--- Eventolia/src/components/components/UserProfile/UserSideUserProfilePage.vue
+++ Eventolia/src/components/components/UserProfile/UserSideUserProfilePage.vue
@@ -1,11 +1,12 @@
 <template>
-
+    <div class="perfil"><h1>Perfil</h1></div>
+    <div class="imagen"><h1>Imagenes</h1></div>
 </template>
+perfilInfo
+<script setup>
+    import RightSideSingUpPage from '@/components/components/Log_Reg/SignUpPage/RightSideSignUpPage.vue'
+</script>
   
-  <script setup>
-
-  </script>
-  
-  <style scoped>
-  </style>
+<style scoped>
+</style>
   
