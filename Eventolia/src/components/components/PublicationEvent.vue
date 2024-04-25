5b0041bb114ff97376e144ed6125be8d67aeb229
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-25 09:53:12 +0200

    fino

--- Eventolia/src/components/components/PublicationEvent.vue
+++ Eventolia/src/components/components/PublicationEvent.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-  <div class="adminEvent">
+    <div class="adminEvent">
+      <div class="perfil-img"></div>
+      <div>
+        <p class="first-text"><b>tom.ybarguengoitia</b></p>
+        <p class="second-text">El Dirty</p>
+      </div>
+
+    </div>
+    <SimplePublication />
   </div>
-  <SimplePublication />
-</div>
 </template>
 
 <script setup>
@@ -12,9 +18,32 @@ import SimplePublication from './SimplePublication.vue'; // Importamos el compon
 
 
 <style scoped>
+.first-text {
+  margin-bottom: 0;
+  /* Reduce el espacio debajo del primer párrafo */
+}
+
+.second-text {
+  font-size: 0.8em;
+  /* Hace que el segundo texto sea más pequeño */
+  margin-top: 0;
+  /* Reduce el espacio encima del segundo párrafo */
+}
+
 .adminEvent {
   width: 450px;
   height: 65px;
-  background-color: red;
+  display: flex;
+  align-items: center;
+  border-radius: 8px 8px 0 0;
 }
-</style>
+
+.perfil-img {
+  margin-left: 10px;
+  width: 40px;
+  height: 40px;
+  background-image: url("../../assets/images/profile2.png");
+  background-size: cover;
+  border-radius: 50%;
+  margin-right: 10px;
+}</style>
