e8bd2f103d4604fc3acd635b194af4147fb355a7
PauChavero <a22pauchagor@inspedralbes.cat>
2024-04-25 13:01:44 +0200

    a

--- Eventolia/src/components/components/UserProfile/InfoUserProfile.vue
+++ Eventolia/src/components/components/UserProfile/InfoUserProfile.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="bg-info">
         <h1>Info User Profile</h1>
+        <div class="img-usuario"><ImagenUsuario/></div>
     </div>
 </template>
 
 <script setup>
-
+    import ImagenUsuario from '@/components/components/UserProfile/imageProfile.vue'
 </script>
+
+<style setup>
+
+
+</style>
---
+++ Eventolia/src/components/components/UserProfile/imageProfile.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="bg-info">
+        <img data-v-1ed81890="" class="img" src="/src/assets/images/rascacielos.jpeg" alt="Imagen de perfil">
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style setup>
+
+.img {
+    width: 200px; /* O el tamaño que desees */
+    height: 200px; /* Debe ser igual al ancho para ser un círculo perfecto */
+    border-radius: 50%; /* Esto hace que la imagen sea circular */
+    object-fit: cover; /* Asegura que la imagen cubra el espacio sin deformarse */
+}
+</style>
