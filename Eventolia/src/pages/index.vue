0a596ffca80996f2fdeac186be67b6b3e3cb3486
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-26 09:50:33 +0200

    ya conseguimso que detecte si es mobile o desktop

--- Eventolia/src/pages/index.vue
+++ Eventolia/src/pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <HelloWorld />
+  <DesktopHelloWorld v-if="isDesktop" />
+  <MobileHelloWorld v-else />
 </template>
 
 <script setup>
-import HelloWorld from '@/components/ViewComponents/Desktop/DesktopHelloWorld.vue';
+import { ref, onMounted, onUnmounted } from 'vue';
+import DesktopHelloWorld from '@/components/ViewComponents/Desktop/DesktopHelloWorld.vue';
+import MobileHelloWorld from '@/components/ViewComponents/Mobile/MobileHelloWorld.vue';
 
-  //
+const isDesktop = ref(window.innerWidth > 800); // Cambia este valor según tus necesidades
+
+const updateWidth = () => {
+  isDesktop.value = window.innerWidth > 800; // Cambia este valor según tus necesidades
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateWidth);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWidth);
+});
 </script>
