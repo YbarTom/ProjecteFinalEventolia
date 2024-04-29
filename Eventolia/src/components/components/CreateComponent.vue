9dfc2fb1dfd40a56b9f2d4595504f4931df32eda
YbarTom <a22tomybanog@inspedralbes.cat>
2024-04-29 11:40:38 +0200

    mejor

---
+++ Eventolia/src/components/components/CreateComponent.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <div class="post bg-principal">
+      <div>
+        <p class="title text-text"><b>Create new post</b></p>
+        <div class="horizontal-bar-create bg-background"></div>
+        <!-- Mover el bloque del botón aquí -->
+      </div>
+      <div class="div-button">
+        <label for="image-upload" class="upload-button bg-background">Select Image From Computer</label>
+        <input id="image-upload" type="file" @change="handleImageUpload" style="display: none;">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const imagePreview = ref('');
+
+const handleImageUpload = (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      imagePreview.value = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+};
+</script>
+
+<style scoped>
+.div-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: auto; /* Esto colocará el botón en la parte inferior del contenedor */
+  padding: 20px 0; /* Añadir espacio alrededor del botón */
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 79%;
+}
+
+.title {
+  font-size: 18px;
+  margin-top: 20px;
+}
+
+.post {
+  width: 80%;
+  text-align: center;
+  border-radius: 16px;
+  height: 80vh;
+  display: flex; /* Añadir esto para permitir el posicionamiento flex */
+  flex-direction: column; /* Añadir esto para que los elementos internos se apilen verticalmente */
+}
+
+.horizontal-bar-create {
+  width: 100%;
+  height: 1px;
+  margin-top: 20px;
+}
+
+.upload-button {
+  font-weight: bold;
+  border-radius: 16px;
+  padding: 10px 20px;
+}
+
+.post img {
+  max-width: 100%;
+  max-height: 300px;
+  margin-top: 10px;
+}
+</style>
