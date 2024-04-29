baefdaf66ddd7a6477a87ce0d590432177e4b63c
PauChavero <a22pauchagor@inspedralbes.cat>
2024-04-29 08:21:00 +0200

    a

--- Eventolia/src/pages/SignUpPage.vue
+++ Eventolia/src/pages/SignUpPage.vue
@@ -1,8 +1,24 @@
 <template>
-  <MainPage/>
+  <SignUpPage v-if="isDesktop" />
+  <MobileSignUpPage v-else />
 </template>
 
 <script setup>
-import MainPage from '@/components/ViewComponents/Desktop/DesktopSignUp.vue';
+import { ref, onMounted, onUnmounted } from 'vue';
+import SignUpPage from '@/components/ViewComponents/Desktop/DesktopSignUp.vue';
+import MobileSignUpPage from '@/components/ViewComponents/Mobile/MobileSignUpPage.vue';
+const isDesktop = ref(window.innerWidth > 800); // Cambia este valor según tus necesidades
+
+const updateWidth = () => {
+  isDesktop.value = window.innerWidth > 800; // Cambia este valor según tus necesidades
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateWidth);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWidth);
+});
 </script>
 
