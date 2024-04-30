bfcc7374abaa8ec7a98a5b67dd8072a4f2b5dc88
PauChavero <a22pauchagor@inspedralbes.cat>
2024-04-30 12:22:25 +0200

    Messatges

--- Eventolia/src/pages/Messages.vue
+++ Eventolia/src/pages/Messages.vue
@@ -1,11 +1,25 @@
 <template>
-    <DesktopMessage/>
-  </template>
-  
-  <script setup>
-  import DesktopMessage from '@/components/ViewComponents/Desktop/DesktopMessage.vue';
-  
-  
-    //
-  </script>
+  <DesktopMessatges v-if="isDesktop" />
+  <MobileMessatges v-else />
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import DesktopMessatges from '@/components/ViewComponents/Desktop/DesktopMessatges.vue';
+import MobileMessatges from '@/components/ViewComponents/Mobile/MobileMessatges.vue';
+const isDesktop = ref(window.innerWidth > 800);
+
+const updateWidth = () => {
+  isDesktop.value = window.innerWidth > 800; 
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateWidth);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWidth);
+});
+
+</script>
   
