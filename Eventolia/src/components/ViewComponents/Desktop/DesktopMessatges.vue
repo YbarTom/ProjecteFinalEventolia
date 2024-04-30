90f0f8a27f4ba1c2f945d03df0c90b353066eeda
PauChavero <a22pauchagor@inspedralbes.cat>
2024-04-30 12:28:05 +0200

    a
    A

--- Eventolia/src/components/ViewComponents/Desktop/DesktopMessatges.vue
+++ Eventolia/src/components/ViewComponents/Desktop/DesktopMessatges.vue
@@ -1,8 +1,13 @@
 <template>
-    
+    <div>
+    <div><LeftSideMessatges/></div>
+    <div><MediumSideMessatges/></div>
+    <div><RightSideMessatges/></div>
+  </div>
 </template>
 
 <script setup>
-
-
+import LeftSideMessatges from "@/components/components/";
+import MediumSideMessatges from "@/components/components/MediumSideMessatges.vue";
+import RightSideMessatges from "@/components/components/RightSideMessatges.vue";
 </script>
---
+++ Eventolia/src/components/components/Message/MediumSideMessatges.vue
@@ -0,0 +1,7 @@
+<template>
+    <div><InfoUserMessage/></div>
+</template>
+
+<script setup>
+
+import InfoUserMessage from "@/components/components/InfoUserMessage.vue";
