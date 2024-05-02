cdfe6684c0212b370ced48e4b460523bad974390
YbarTom <a22tomybanog@inspedralbes.cat>
2024-05-02 09:11:26 +0200

    search bar

---
+++ Eventolia/src/components/components/SearchComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container bg-warning">
+        <v-autocomplete
+            :items="items"
+            append-inner-icon="mdi-microphone"
+            class="mx-auto"
+            density="comfortable"
+            menu-icon=""
+            placeholder="Search Google or type a URL"
+            prepend-inner-icon="mdi-magnify"
+            style="max-width: 350px;"
+            theme="light"
+            variant="solo"
+            auto-select-first
+            item-props
+            rounded
+          ></v-autocomplete>
+    </div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const items = ref([
+    {
+      prependIcon: 'mdi-clock-outline',
+      title: 'recipe with chicken',
+    },
+    {
+      prependIcon: 'mdi-clock-outline',
+      title: 'best hiking trails near me',
+    },
+    {
+      prependIcon: 'mdi-clock-outline',
+      title: 'how to learn a new language',
+    },
+    {
+      prependIcon: 'mdi-clock-outline',
+      title: 'DIY home organization ideas',
+    },
+    {
+      prependIcon: 'mdi-clock-outline',
+      title: 'latest fashion trends',
+    },
+  ]);
+
+</script>
+
+
+<style scoped>
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 79%;
+}
+
+
+</style>
