216a94446fa5e73ec6b5f80a1fb8ce4691418576
YbarTom <a22tomybanog@inspedralbes.cat>
2024-05-02 09:57:42 +0200

    search bar

--- Eventolia/src/components/components/SearchComponent.vue
+++ Eventolia/src/components/components/SearchComponent.vue
@@ -1,59 +1,53 @@
 <template>
-    <div class="container bg-warning">
-        <!--para el icono al final de la barra de busquedea append-inner-icon="mdi-microphone"-->
-        <v-autocomplete
-            :items="items"
-            class="mx-auto"
-            density="comfortable"
-            menu-icon=""
-            placeholder="Search Google or type a URL"
-            prepend-inner-icon="mdi-magnify"
-            theme="light"
-            variant="solo"
-            auto-select-first
-            item-props
-            rounded
-          ></v-autocomplete>
+  <div class="container ">
+    <!--para el icono al final de la barra de busquedea append-inner-icon="mdi-microphone"-->
+    <div class="div-top">
+      <v-autocomplete :items="items" class="mx-auto" density="comfortable" menu-icon=""
+        placeholder="Search Google or type a URL" prepend-inner-icon="mdi-magnify" variant="solo" auto-select-first
+        item-props rounded></v-autocomplete>
     </div>
+  </div>
 </template>
 <script setup>
 import { ref, onMounted } from 'vue';
 
 const items = ref([
-    {
-      prependIcon: 'mdi-clock-outline',
-      title: 'recipe with chicken',
-    },
-    {
-      prependIcon: 'mdi-clock-outline',
-      title: 'best hiking trails near me',
-    },
-    {
-      prependIcon: 'mdi-clock-outline',
-      title: 'how to learn a new language',
-    },
-    {
-      prependIcon: 'mdi-clock-outline',
-      title: 'DIY home organization ideas',
-    },
-    {
-      prependIcon: 'mdi-clock-outline',
-      title: 'latest fashion trends',
-    },
-  ]);
+  {
+    prependIcon: 'mdi-clock-outline',
+    title: 'recipe with chicken',
+  },
+  {
+    prependIcon: 'mdi-clock-outline',
+    title: 'best hiking trails near me',
+  },
+  {
+    prependIcon: 'mdi-clock-outline',
+    title: 'how to learn a new language',
+  },
+  {
+    prependIcon: 'mdi-clock-outline',
+    title: 'DIY home organization ideas',
+  },
+  {
+    prependIcon: 'mdi-clock-outline',
+    title: 'latest fashion trends',
+  },
+]);
 
 </script>
 
 
 <style scoped>
-
-.container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 79%;
+.div-top{
+  width: 100%;
+  align-self: flex-start; /* Esto hará que el div esté tocando la parte superior */
+  margin-top: 35px; /* Esto hará que el div esté tocando la parte superior */
 }
-
-
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 79%;
+}
 </style>
