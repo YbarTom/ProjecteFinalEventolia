7cc8f86c6f1e80921e78b1717b9920c1e06c3dc7
YbarTom <a22tomybanog@inspedralbes.cat>
2024-05-02 10:24:45 +0200

    grid

--- Eventolia/src/components/components/SearchComponent.vue
+++ Eventolia/src/components/components/SearchComponent.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="container ">
-    <!--para el icono al final de la barra de busquedea append-inner-icon="mdi-microphone"-->
+  <div class="container">
     <div class="div-top">
       <v-autocomplete :items="items" class="mx-auto" density="comfortable" menu-icon=""
         placeholder="Search Google or type a URL" prepend-inner-icon="mdi-magnify" variant="solo" auto-select-first
         item-props rounded></v-autocomplete>
     </div>
+    <div class="grid">
+      <!-- Aquí puedes agregar el contenido de las columnas -->
+    </div>
   </div>
 </template>
+
 <script setup>
 import { ref, onMounted } from 'vue';
 
@@ -33,21 +36,28 @@ const items = ref([
     title: 'latest fashion trends',
   },
 ]);
-
 </script>
 
-
 <style scoped>
-.div-top{
+.div-top {
   width: 100%;
-  align-self: flex-start; /* Esto hará que el div esté tocando la parte superior */
-  margin-top: 35px; /* Esto hará que el div esté tocando la parte superior */
+  align-self: flex-start;
+  margin-top: 35px;
 }
 .container {
   display: flex;
-  justify-content: center;
+  flex-direction: column; /* Cambiado de row a column */
   align-items: center;
   height: 100vh;
   width: 79%;
+}
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  background-color: red;
+  margin-top: 20px; /* Agregado margen superior */
+  height: 100vh;
+  width: 100%;
 }
 </style>
