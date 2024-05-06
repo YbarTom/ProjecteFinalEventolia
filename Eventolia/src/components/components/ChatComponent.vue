41a397a0a3e2c23e507c1e30eb556ed2f44b4025
YbarTom <a22tomybanog@inspedralbes.cat>
2024-05-06 08:49:16 +0200

    chatcomponent

---
+++ Eventolia/src/components/components/ChatComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 79%;
+  background-color: red;
+}
+</style>
