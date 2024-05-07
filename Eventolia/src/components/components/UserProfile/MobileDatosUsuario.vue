6996b6db230204c729fc985d03f6f7e9f880e687
PauChavero <a22pauchagor@inspedralbes.cat>
2024-05-07 20:47:30 +0200

    A
    Estructura a falta de añadir la estructura de las imagenes

--- Eventolia/src/components/components/UserProfile/MobileDatosUsuario.vue
+++ Eventolia/src/components/components/UserProfile/MobileDatosUsuario.vue
@@ -1,60 +1,87 @@
 <template>
-    
-  </template>
-  
-  <script setup>
-  import ImagenUsuario from '@/components/components/foto.vue'
-  import BtnSeguir from '@/components/components/UserProfile/btnSeguir.vue'
-  import PopUpUsers from './PopUpUsers.vue';
-  import { defineProps } from "vue";
-  import * as funcionsCM from '@/communicationsManager.js'
-  
-  const props = defineProps({
+    <div class="todo">
+        <ImagenUsuario />
+        <div class="usuario-info">
+            <h1>{{ props.userProfile.userName }}</h1>
+        </div>
+        <div class="numeros">
+            <div>
+                <button @click="mostrarPopUp(props.userProfile.followed, 1)">
+                    <h4>Seguidos</h4>
+                    <h3>{{ props.userProfile.followed.length }}</h3>
+                </button>
+            </div>
+            <div>
+                <button @click="mostrarPopUp(props.userProfile.followers, 2)">
+                    <h4>Seguidores</h4>
+                    <h3>{{ props.userProfile.followers.length }}</h3>
+                </button>
+            </div>
+            <div>
+                <h4>Publicaciones</h4>
+                <h3>{{ props.userProfile.posts.length + props.userProfile.events.length }}</h3>
+            </div>
+        </div>
+        <div class="btn-seguir">
+            <BtnSeguir />
+        </div>
+        
+    </div>
+</template>
+
+<script setup>
+import ImagenUsuario from '@/components/components/foto.vue'
+import BtnSeguir from '@/components/components/UserProfile/btnSeguir.vue'
+import PopUpUsers from './PopUpUsers.vue';
+import { defineProps } from "vue";
+import * as funcionsCM from '@/communicationsManager.js'
+
+const props = defineProps({
     userProfile: Object,
     ownProfile: Boolean
-  })
-  
-  const showPopUp = ref(false);
-  const typePopUp = ref(0)
-  const followers = ref([]);
-  const followed = ref([])
-  
-  const mostrarPopUp = async (users, type) => {
-  
+})
+
+const showPopUp = ref(false);
+const typePopUp = ref(0)
+const followers = ref([]);
+const followed = ref([])
+
+const mostrarPopUp = async (users, type) => {
+
     if (type === 1) {
-      followed.value = await funcionsCM.getFollowed(users)
-      typePopUp.value = 1
+        followed.value = await funcionsCM.getFollowed(users)
+        typePopUp.value = 1
     }
     else if (type === 2) {
-      followers.value = await funcionsCM.getFollowers(users)
-      typePopUp.value = 2
+        followers.value = await funcionsCM.getFollowers(users)
+        typePopUp.value = 2
     }
     showPopUp.value = true;
-  }
-  </script>
-  
-  
-  <style scoped>
-  .usuario-info {
+}
+</script>
+
+<style scoped>
+.todo {
     display: flex;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
-  }
-  
-  .usuario-info h1 {
-    margin-left: 20px;
-    text-align: center;
-    flex-grow: 1;
-    /* Permite que el título ocupe todo el espacio restante */
-  }
-  
-  .numeros-usu {
+    height: 100vh;
+    margin-left: 100px;
+}
+
+.numeros {
     display: flex;
     justify-content: space-around;
-    /* Distribuye los elementos con espacio equitativo */
-  }
-  
-  .numeros-usu>div {
-    margin: 10px;
-  }
-  </style>
-  
+    align-items: center;
+}
+
+.numeros>div {
+    margin: 0 10px;
+}
+
+.numeros h3,
+.numeros h4 {
+    font-size: calc(1em + 1vw);
+}
+</style>
