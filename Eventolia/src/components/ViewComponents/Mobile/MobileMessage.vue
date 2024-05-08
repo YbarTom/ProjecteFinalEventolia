364e3af1c566a68973bc6695906d70f073f0993f
PauChavero <a22pauchagor@inspedralbes.cat>
2024-05-08 09:27:40 +0200

    M

---
+++ Eventolia/src/components/ViewComponents/Mobile/MobileMessage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+      <div class="left-panel"><LeftSideMainPage/></div>
+      <div class="middle-panel "><ChatComponent/></div>
+    </div>
+  </template>
+  
+  <script setup>
+  import LeftSideMainPage from "@/components/components/LeftSideMainPage.vue";
+  import ChatComponent from "@/components/components/ChatComponent.vue";
+  </script>
+  
+  <style scoped>
+  .container {
+    display: flex;
+    height: 100vh;
+    overflow: hidden;
+  }
+  
+  .left-panel {
+    flex: 1;
+  }
+  
+  .middle-panel {
+    flex: 2; /* Ocupa los dos tercios del contenedor */
+  }
+  </style>
+  
--- Eventolia/src/pages/Messages.vue
+++ Eventolia/src/pages/Messages.vue
@@ -1,11 +1,27 @@
 <template>
-    <DesktopMessage/>
+    <DesktopMessage v-if="isDesktop"/>
+    <Messages v-else/>
   </template>
   
   <script setup>
+  import { ref, onMounted, onUnmounted } from 'vue';
+
   import DesktopMessage from '@/components/ViewComponents/Desktop/DesktopMessage.vue';
   
   
+  const isDesktop = ref(window.innerWidth > 800);
+
+const updateWidth = () => {
+  isDesktop.value = window.innerWidth > 800; 
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateWidth);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWidth);
+});
     //
   </script>
   
