11e99025edf798a0561037ea580a6448df326089
PauChavero <a22pauchagor@inspedralbes.cat>
2024-05-09 10:39:12 +0200

    a

--- Eventolia/src/pages/Search.vue
+++ Eventolia/src/pages/Search.vue
@@ -1,10 +1,26 @@
 <template>
-    <DesktopSearch />
+    <DesktopSearch v-if="isDesktop" />
+    <MobileSearch v-else/>
 </template>
-
 <script setup>
-import DesktopSearch from '../components/ViewComponents/Desktop/DesktopSearch.vue';
+  import { ref, onMounted, onUnmounted } from 'vue';
 
+  const isDesktop = ref(window.innerWidth > 800);
+
+const updateWidth = () => {
+  isDesktop.value = window.innerWidth > 800; 
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateWidth);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWidth);
+});
+
+import DesktopSearch from '../components/ViewComponents/Desktop/DesktopSearch.vue';
+import MobileSearch from '../components/ViewComponents/Mobile/MobileSearch.vue';
 
 //
 </script>
