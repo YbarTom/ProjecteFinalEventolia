1ff62a6871161493383a3312e525932b6f1191a3
PauChavero <a22pauchagor@inspedralbes.cat>
2024-05-09 10:51:03 +0200

    a

--- Eventolia/src/pages/Search.vue
+++ Eventolia/src/pages/Search.vue
@@ -1,26 +1,28 @@
 <template>
     <DesktopSearch v-if="isDesktop" />
-    <MobileSearch v-else/>
+    <MobileSearch v-else />
 </template>
 <script setup>
-  import { ref, onMounted, onUnmounted } from 'vue';
-
-  const isDesktop = ref(window.innerWidth > 800);
-
-const updateWidth = () => {
-  isDesktop.value = window.innerWidth > 800; 
-};
-
-onMounted(() => {
-  window.addEventListener('resize', updateWidth);
-});
-
-onUnmounted(() => {
-  window.removeEventListener('resize', updateWidth);
-});
+import { ref, onMounted, onUnmounted } from 'vue';
 
 import DesktopSearch from '../components/ViewComponents/Desktop/DesktopSearch.vue';
 import MobileSearch from '../components/ViewComponents/Mobile/MobileSearch.vue';
+
+const isDesktop = ref(window.innerWidth > 800);
+
+const updateWidth = () => {
+    isDesktop.value = window.innerWidth > 800;
+};
+
+onMounted(() => {
+    window.addEventListener('resize', updateWidth);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('resize', updateWidth);
+});
+
+
 
 //
 </script>
