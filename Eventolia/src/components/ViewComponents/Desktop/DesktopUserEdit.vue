eeff33fe50a00936e25d20c0d1945a2c2e5d57db
MikiDix <a22miqdervil@inspedralbes.cat>
2024-05-13 12:39:25 +0200

    edit username/password

--- Eventolia/src/components/ViewComponents/Desktop/DesktopUserEdit.vue
+++ Eventolia/src/components/ViewComponents/Desktop/DesktopUserEdit.vue
@@ -1,17 +1,19 @@
 <template>
 
-    <TextField v-model="password" text="New Password"/>
-    <TextField v-model="password2" text="Repeat Password"/>
+    <TextField v-model="password" text="New Password" />
+    <TextField v-model="password2" text="Repeat Password" />
 
-    <TextField v-model="userName" text="New UserName"/>
+    <TextField v-model="userName" text="New UserName" />
 
-    <button @click="editUser" >Edit</button>
+    <button @click="editUser">Edit</button>
 
     <p v-if="checkError">{{ errorMessage }}</p>
 </template>
 <script setup>
+import { ref } from 'vue'
 import TextField from '@/components/components/Log_Reg/TextField.vue'
 import { useAppStore } from '@/stores/app';
+import * as funcionsCM from '@/communicationsManager.js'
 
 const appStore = useAppStore()
 const password = ref("")
@@ -20,27 +22,28 @@ const userName = ref("")
 const errorMessage = ref("")
 const checkError = ref(false)
 
-async function editUser(){
-    if(password === "" && userName !== ""){
-        funcionsCM.editUserName(userName)
+async function editUser() {
+
+    if (password.value === "" && userName.value !== "") {
+        funcionsCM.editUserName({idUser: appStore.getUserId(), userName: userName.value})
     }
-    else if(password !== "" && userName !== ""){
-        if(password !== password2){
+    else if (password.value !== "" && userName.value !== "") {
+        if (password.value !== password2.value) {
             errorMessage.value = "The passwords are not the same"
             checkError.value = true
         }
         else {
-            funcionsCM.editPassword(password)
-            funcionsCM.editUserName(userName)
+            funcionsCM.editPassword({idUser: appStore.getUserId(), password: password.value})
+            funcionsCM.editUserName({idUser: appStore.getUserId(), userName: userName.value})
         }
     }
-    else if(password !== "" && userName === ""){
-        if(password !== password2){
+    else if (password.value !== "" && userName.value === "") {
+        if (password.value !== password2.value) {
             errorMessage.value = "The passwords are not the same"
             checkError.value = true
         }
         else {
-            funcionsCM.editPassword(password)
+            funcionsCM.editPassword({idUser: appStore.getUserId(), password: password.value})
         }
     }
 }
