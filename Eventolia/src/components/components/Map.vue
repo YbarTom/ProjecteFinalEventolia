d4a9387bb84722e27a752a74c7436ad26f5994db
YbarTom <a22tomybanog@inspedralbes.cat>
2024-05-13 09:37:01 +0200

    asi mejor

--- Eventolia/src/components/components/Map.vue
+++ Eventolia/src/components/components/Map.vue
@@ -1,5 +1,15 @@
 <template>
-    <div id="map"></div>
+    <div>
+      <div id="map"></div>
+      <label for="radiusSelect">Selecciona el radio en kilómetros:</label>
+      <select id="radiusSelect" v-model="selectedRadius" @change="updateCircleRadius">
+        <option value="1">1 km</option>
+        <option value="2">2 km</option>
+        <option value="5">5 km</option>
+        <option value="10">10 km</option>
+        <!-- Agrega más opciones según sea necesario -->
+      </select>
+    </div>
   </template>
   
   <script>
@@ -8,6 +18,15 @@
   import * as funcionsCM from '../../communicationsManager.js';
   
   export default {
+    data() {
+      return {
+        selectedRadius: '5', // Valor predeterminado del selector
+        map: null,
+        circle: null,
+        latitude: 0, // Variables para almacenar las coordenadas del usuario
+        longitude: 0,
+      };
+    },
     mounted() {
       // Obtener los eventos antes de inicializar el mapa
       funcionsCM.getEvents()
@@ -22,45 +41,50 @@
       initMap(events) {
         // Inicializar el mapa
         const appStore = useAppStore();
-        const latitude = appStore.getUserLatitude();
-        const longitude = appStore.getUserLongitude();
-        const map = L.map('map').setView([latitude, longitude], 13);
+        this.latitude = appStore.getUserLatitude(); // Asignar las coordenadas del usuario a las variables de datos
+        this.longitude = appStore.getUserLongitude();
+        this.map = L.map('map').setView([this.latitude, this.longitude], 13);
   
         // Añadir capa de OpenStreetMap
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '© OpenStreetMap contributors'
-        }).addTo(map);
+        }).addTo(this.map);
   
+        // Añadir marcador del usuario
         const redIcon = L.icon({
-                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-                iconSize: [25, 41],
-                iconAnchor: [12, 41],
-                popupAnchor: [1, -34],
-                shadowSize: [41, 41],
-            });
-
-            // Añadir marcador del usuario con el icono rojo
-            L.marker([latitude, longitude], { icon: redIcon }).addTo(map)
-                
+          iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+          popupAnchor: [1, -34],
+          shadowSize: [41, 41],
+        });
+        L.marker([this.latitude, this.longitude], { icon: redIcon }).addTo(this.map);
   
         // Añadir círculo alrededor del usuario para representar el área
-        const radiusInKilometers = 5; // Cambia este valor según tus necesidades
-        const circle = L.circle([latitude, longitude], {
-          color: 'red',
-          fillColor: '#f03',
-          fillOpacity: 0.5,
-          radius: radiusInKilometers * 1000 // Convertir kilómetros a metros
-        }).addTo(map);
+        this.addCircle();
   
         // Añadir marcadores de eventos
         events.forEach(event => {
-          L.marker([event.latitude, event.longitude]).addTo(map)
+          L.marker([event.latitude, event.longitude]).addTo(this.map)
             .bindPopup(event.title);
         });
   
         // Ajustar vista del mapa para incluir todos los elementos
-        const bounds = L.latLngBounds([latitude, longitude], circle.getBounds().getNorthEast());
-        map.fitBounds(bounds);
+        const bounds = L.latLngBounds([this.latitude, this.longitude], this.circle.getBounds().getNorthEast());
+        this.map.fitBounds(bounds);
+      },
+      addCircle() {
+        const radiusInKilometers = parseInt(this.selectedRadius);
+        this.circle = L.circle([this.latitude, this.longitude], {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.2,
+          radius: radiusInKilometers * 1000 // Convertir kilómetros a metros
+        }).addTo(this.map);
+      },
+      updateCircleRadius() {
+        const radiusInKilometers = parseInt(this.selectedRadius);
+        this.circle.setRadius(radiusInKilometers * 1000); // Convertir kilómetros a metros
       },
     },
   };
