0392e4fd69ab7ce101d8b897d59552e2b00dde4a
PauChavero <a22pauchagor@inspedralbes.cat>
2024-05-16 08:59:19 +0200

    UserProfile

--- Eventolia/src/components/components/UserProfile/UserSideUserProfilePage.vue
+++ Eventolia/src/components/components/UserProfile/UserSideUserProfilePage.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-        <PanelProfile :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="props.events" :posts="props.posts"/>
+        <PanelProfile v-if="isDesktop" :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="props.events" :posts="props.posts"/>
+        <PanelProfileMobile v-else  :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="props.events" :posts="props.posts"/>
     </div>
 
 </template>
 <script setup>
 import PanelProfile from '@/components/components/UserProfile/PanelProfile.vue'
+
+import PanelProfileMobile from '@/components/components/UserProfile/PanelProfileMobil.vue'
 import { defineProps } from "vue";
 const props = defineProps({
     userProfile: Object,
