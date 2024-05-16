96a52ed497327733d7982ef0478b0b7598e4b789
MikiDix <a22miqdervil@inspedralbes.cat>
2024-05-16 12:18:26 +0200

    el text de les notis es crea bé

--- Eventolia/src/components/ViewComponents/Desktop/DesktopNotification.vue
+++ Eventolia/src/components/ViewComponents/Desktop/DesktopNotification.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <p v-for="notification in props.notifications">{{ notification.notificator }}</p>
+        <p v-for="notification in notificationsMessage">{{ notification }}</p>
     </div>
 </template>
 
@@ -11,7 +11,35 @@ const props = defineProps({
     notifications: Array
 })
 
-console.log(props.notifications[0])
+const notificationsMessage = ref([])
+
+console.log(props.notifications)
+
+onMounted(async () => {
+    var message
+    try {
+        for (let i = 0; i < props.notifications.length; i++) {
+            switch (props.notifications[i].type) {
+                case "follow":
+                    message = props.notifications[i].notificator + " has followed you"
+                    break;
+                case "likePost":
+                    message = props.notifications[i].notificator + " has liked your post"
+                    break;
+                case "newComment":
+                    message = props.notifications[i].notificator + " has commented on your post: " + props.notifications[i].text
+                    break;
+                case "newAssist":
+                    message = props.notifications[i].notificator + " has confirmed assistance to your event"
+                    break;
+            }
+            notificationsMessage.value.push(message)
+        }
+
+    } catch (error) {
+        console.error(error)
+    }
+})
 </script>
 
 <style scoped></style>
