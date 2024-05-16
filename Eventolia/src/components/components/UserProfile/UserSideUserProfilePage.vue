0a4b4d406d0a25c906a133180ac460648a8b0891
PauChavero <a22pauchagor@inspedralbes.cat>
2024-05-16 10:41:06 +0200

    a

--- Eventolia/src/components/components/UserProfile/UserSideUserProfilePage.vue
+++ Eventolia/src/components/components/UserProfile/UserSideUserProfilePage.vue
@@ -1,23 +1,25 @@
 <template>
     <div v-if="isDesktop">
-        <PanelProfile  :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="props.events" :posts="props.posts"/>
-    
+        <PanelProfile :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="props.events"
+            :posts="props.posts" />
     </div>
-        <div v-else>
-
-    
-        <PanelProfileMobile   :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="props.events" :posts="props.posts"/>
+    <div v-else>
+        <PanelProfileMobile :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="props.events"
+            :posts="props.posts" />
     </div>
 
 </template>
 <script setup>
-const isDesktop = ref(window.innerWidth > 800);
 
 
 import PanelProfile from '@/components/components/UserProfile/PanelProfile.vue'
-
 import PanelProfileMobile from '@/components/components/UserProfile/PanelProfileMobil.vue'
+
+
 import { defineProps } from "vue";
+
+
+const isDesktop = ref(window.innerWidth > 800);
 const props = defineProps({
     userProfile: Object,
     ownProfile: Boolean,
