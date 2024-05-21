5bfa7ced5e406c54239169a86b141f9a306356cf
MikiDix <a22miqdervil@inspedralbes.cat>
2024-05-21 08:38:16 +0200

    notis millor

--- Eventolia/src/components/ViewComponents/Desktop/DesktopNotification.vue
+++ Eventolia/src/components/ViewComponents/Desktop/DesktopNotification.vue
@@ -1,43 +1,41 @@
 <template>
     <div>
-        <p v-for="notification in notificationsMessage">{{ notification }}</p>
+        <p v-for="(notification, index) in notificationsMessage" :key="index">{{ notification }}</p>
     </div>
 </template>
 
 <script setup>
-import { defineProps, onMounted, ref } from 'vue';
+import { defineProps, computed } from 'vue';
 
 const props = defineProps({
-    notifications: Array
-})
-
-const notificationsMessage = ref([])
-
-onMounted(async () => {
-    var message
-    try {
-        for (let i = 0; i < props.notifications.length; i++) {
-            switch (props.notifications[i].type) {
-                case "follow":
-                    message = props.notifications[i].notificator + " has followed you"
-                    break;
-                case "likePost":
-                    message = props.notifications[i].notificator + " has liked your post"
-                    break;
-                case "newComment":
-                    message = props.notifications[i].notificator + " has commented on your post: " + props.notifications[i].text
-                    break;
-                case "newAssist":
-                    message = props.notifications[i].notificator + " has confirmed assistance to your event"
-                    break;
-            }
-            notificationsMessage.value.push(message)
-        }
-
-    } catch (error) {
-        console.error(error)
+    notifications: {
+        type: Array,
+        required: true
     }
 })
+
+const notificationsMessage = computed(() => {
+    return props.notifications.map(notification => {
+        let message = "";
+        switch (notification.type) {
+            case "follow":
+                message = `${notification.notificator} has followed you`;
+                break;
+            case "likePost":
+                message = `${notification.notificator} has liked your post`;
+                break;
+            case "newComment":
+                message = `${notification.notificator} has commented on your post: ${notification.text}`;
+                break;
+            case "newAssist":
+                message = `${notification.notificator} has confirmed assistance to your event`;
+                break;
+            default:
+                message = `Unknown notification type`;
+        }
+        return message;
+    });
+});
 </script>
 
 <style scoped></style>
