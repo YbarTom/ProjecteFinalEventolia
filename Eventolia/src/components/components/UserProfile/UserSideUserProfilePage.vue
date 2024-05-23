8c743fd8edc85d0d744ac6db7595573c36e6aa28
MikiDix <a22miqdervil@inspedralbes.cat>
2024-05-23 12:48:40 +0200

    mes nous primer en el profile

--- Eventolia/src/components/components/UserProfile/UserSideUserProfilePage.vue
+++ Eventolia/src/components/components/UserProfile/UserSideUserProfilePage.vue
@@ -1,23 +1,27 @@
 <template>
     <div v-if="isDesktop">
-        <PanelProfile :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="props.events"
-            :posts="props.posts" />
+        <PanelProfile :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="eventsGrid"
+            :posts="postsGrid" />
     </div>
     <div v-else>
-        <PanelProfileMobile :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="props.events"
-            :posts="props.posts" />
+        <PanelProfileMobile :userProfile="props.userProfile" :ownProfile="props.ownProfile" :events="eventsGrid"
+            :posts="postsGrid" />
     </div>
 
 </template>
 <script setup>
-
-
 import PanelProfile from '@/components/components/UserProfile/PanelProfile.vue'
 import PanelProfileMobile from '@/components/components/UserProfile/PanelProfileMobile.vue'
 
+import { defineProps, onMounted } from "vue";
 
-import { defineProps } from "vue";
+const postsGrid = ref([])
+const eventsGrid = ref([])
 
+onMounted(() => {
+    postsGrid.value = props.posts.reverse()
+    eventsGrid.value = props.events.reverse() 
+});
 
 const isDesktop = ref(window.innerWidth > 800);
 const props = defineProps({
